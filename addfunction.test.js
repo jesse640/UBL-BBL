6840da43a3129e48ee323831be0101e3508f6568
Jesse Chan <z5477899@ad.unsw.edu.au>
2025-03-07 17:33:00 +1100

    fix lint

--- addfunction.test.js
+++ addfunction.test.js
@@ -6,7 +6,7 @@ test('check add function works properly', () => {
 
 test('adds -1 + 1 to equal 0', () => {
   expect(add(-1, 1)).toBe(0)
-});
+})
 
 test('add 5 and 6', () => {
   expect(add(5, 6)).toBe(11)
